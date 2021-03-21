256c5f27d3f4244c81368db6eee030a08a519f34
Samulix20 <samulix2000@gmail.com>
2021-03-21 19:22:39 +0100

    Funcion de modificar usuario implementada

--- baseDatos/BD.go
+++ baseDatos/BD.go
@@ -26,9 +26,10 @@ const (
 	ErrorLeerIconosTienda   = iota
 	ErrorTipoIncorrecto     = iota
 	ErrorBusquedaUsuario    = iota
+	ErrorModificarUsuario   = iota
 )
 
-// Consultas
+// Consultas SQL
 const (
 	crearUsuario = "INSERT INTO usuario (aspecto, icono, nombre, correo, clave," +
 		" riskos, recibeCorreos) VALUES (1, 1, $1, $2, $3, 0, $4) " +
@@ -47,8 +48,11 @@ const (
 	consultaIconosUsuario = "SELECT icono.id_icono AS id, icono.precio AS precio " +
 		"FROM icono INNER JOIN iconoscomprados ON icono.id_icono = iconoscomprados.id_icono " +
 		"WHERE iconoscomprados.id_usuario = "
+	actualizarUsuarioInicio = "UPDATE usuario SET "
+	actualizarUsuarioFin    = " = $1 WHERE id_usuario = $2 AND clave = $3"
 )
 
+// NuevaBD crea una nueva conexion a la base de datos bbdd y la formatea
 func NuevaBD(bbdd string) (*BD, error) {
 	db, err := sql.Open("postgres", bbdd)
 	if err != nil {
@@ -59,9 +63,19 @@ func NuevaBD(bbdd string) (*BD, error) {
 	return &BD{bbdd: bbdd, bd: db}, err
 }
 
+// Cerrar cierra la conexion con la base de datos
+func (b *BD) Cerrar() {
+	b.bd.Close()
+}
+
+// CrearCuenta crea una nueva cuenta con nombre, correo, clave, recibeCorreos y
+// los valores por defecto de riskos, iconos y aspectos. Devuelve todos los datos del
+// nuevo usuario creado
 func (b *BD) CrearCuenta(nombre, correo, clave string,
 	recibeCorreos bool) mensajes.JsonData {
 
+	// Iniciar una transaccion, solo se modifican las tablas si se modifican
+	// todas
 	ctx := context.Background()
 	tx, err := b.bd.BeginTx(ctx, nil)
 	if err != nil {
@@ -88,10 +102,22 @@ func (b *BD) CrearCuenta(nombre, correo, clave string,
 	if err != nil {
 		return mensajes.ErrorJson(err.Error(), ErrorCrearCuenta)
 	}
-
+	// Fin de la transaccion
 	return b.leerDatosUsuario(id, 1, 1, 0, nombre, correo, clave, recibeCorreos)
 }
 
+// ModificarUsuario cambia el valor del campo del usuario(id, clave) por el nuevo valor
+// Si ocurre algun error devuelve el error en formato json
+func (b *BD) ModificarUsuario(id int, clave, campo string, valor interface{}) mensajes.JsonData {
+	_, err := b.bd.Exec(actualizarUsuarioInicio+campo+actualizarUsuarioFin, valor, id, clave)
+	if err != nil {
+		return mensajes.ErrorJson(err.Error(), ErrorModificarUsuario)
+	}
+	return mensajes.ErrorJson("", 0)
+}
+
+// ObtenerUsuario devuelve todos los datos del usuario(id, clave) en formato json
+// Si ocurre algun error devuelve el error en formato json
 func (b *BD) ObtenerUsuario(id int, clave string) mensajes.JsonData {
 	var aspecto, icono, riskos int
 	var recibeCorreos bool
@@ -104,6 +130,8 @@ func (b *BD) ObtenerUsuario(id int, clave string) mensajes.JsonData {
 	return b.leerDatosUsuario(id, icono, aspecto, riskos, nombre, correo, clave, recibeCorreos)
 }
 
+// leerDatosUsuario devuelve el mensaje en formato json con todos los datos de un usuario.
+// Si ocurre algun error devuelve el error en formato json
 func (b *BD) leerDatosUsuario(id, icono, aspecto, riskos int, nombre, correo,
 	clave string, recibeCorreos bool) mensajes.JsonData {
 
@@ -132,6 +160,8 @@ func (b *BD) leerDatosUsuario(id, icono, aspecto, riskos int, nombre, correo,
 	}
 }
 
+// leerCosmetico devuelve en formato json los cosmeticos obtenidos de consulta
+// El json tiene el siguiente formato: {[ "id":int, "precio"int ]}
 func (b *BD) leerCosmetico(consulta string) ([]mensajes.JsonData, error) {
 	filas, err := b.bd.Query(consulta)
 	if err != nil {
@@ -148,6 +178,7 @@ func (b *BD) leerCosmetico(consulta string) ([]mensajes.JsonData, error) {
 	return cosmeticos, nil
 }
 
+// execScript ejecuta el script en la base de datos db
 func execScript(db *sql.DB, script string) {
 	file, err := ioutil.ReadFile(script)
 	if err != nil {
--- test/server_test.go
+++ test/server_test.go
@@ -5,17 +5,16 @@ import (
 	"PS_Risk_server/mensajes"
 	"crypto/sha256"
 	"encoding/json"
-	"fmt"
 	"testing"
 )
 
 const (
 	// Pon aqui la url de la base de datos de Heroku
-	baseDeDatos   = "..."
-	nombre        = "usuario0"
-	correo        = "correo@mail.com"
-	clave         = "6d5074b4bf2b913866157d7674f1eda042c5c614876de876f7512702d2572a06"
-	recibeCorreos = true
+	baseDeDatos       = "postgres://ehoyoqkpcgqyrc:c0f640db53a47820e660f19d987b013d7b01d9ac217b669f971070926969dfe0@ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d3cnglhgpo3rgk"
+	nombreTest        = "usuario0"
+	correoTest        = "correo@mail.com"
+	claveTest         = "6d5074b4bf2b913866157d7674f1eda042c5c614876de876f7512702d2572a06"
+	recibeCorreosTest = true
 )
 
 // NewSHA256 ...
@@ -26,24 +25,50 @@ func NewSHA256(data []byte) []byte {
 
 func TestCrearCuenta(t *testing.T) {
 	bd := baseDatos.NuevaBDConexionLocal(baseDeDatos, true)
-	idEsperado := bd.LeerMaxIdUsuario() + 1
-	fmt.Println("id esperado:", idEsperado)
-	obtenido := bd.CrearCuenta(nombre, correo, clave, recibeCorreos)
-	coincide, esperado := coincideUsuario(nombre, correo, clave,
-		recibeCorreos, idEsperado, obtenido)
+	obtenido := bd.CrearCuenta(nombreTest, correoTest, claveTest, recibeCorreosTest)
+	coincide, esperado := coincideUsuario(nombreTest, correoTest, claveTest, recibeCorreosTest, 1, obtenido)
 	if !coincide {
 		t.Errorf("CrearCuenta() = %q, se esperaba %q", obtenido, esperado)
 	}
+	bd.Cerrar()
 }
 
 func TestActualizarUsuario(t *testing.T) {
 	bd := baseDatos.NuevaBDConexionLocal(baseDeDatos, false)
-	obtenido := bd.ObtenerUsuario(1, clave)
-	coincide, esperado := coincideUsuario(nombre, correo, clave,
-		recibeCorreos, 1, obtenido)
+	obtenido := bd.ObtenerUsuario(1, claveTest)
+	coincide, esperado := coincideUsuario(nombreTest, correoTest, claveTest, recibeCorreosTest, 1, obtenido)
 	if !coincide {
 		t.Errorf("CrearCuenta() = %q, se esperaba %q", obtenido, esperado)
 	}
+	bd.Cerrar()
+}
+
+func TestModificarUsuario(t *testing.T) {
+	nuevoNombre := "nuevo Nombre"
+	nuevoCorreo := "nuevoCorreo@mail.com"
+	nuevaClave := "82ef67bb06675af2d43639806236ad1189253ce86e6210c072a3a265987df429"
+
+	bd := baseDatos.NuevaBDConexionLocal(baseDeDatos, false)
+
+	obtenido := bd.ModificarUsuario(1, claveTest, "clave", nuevaClave)
+	if obtenido["code"].(int) != 0 {
+		t.Errorf("Error obtenido modificando usuario, %q", obtenido["err"])
+	}
+	obtenido = bd.ModificarUsuario(1, nuevaClave, "nombre", nuevoNombre)
+	if obtenido["code"].(int) != 0 {
+		t.Errorf("Error obtenido modificando usuario, %q", obtenido["err"])
+	}
+	obtenido = bd.ModificarUsuario(1, nuevaClave, "correo", nuevoCorreo)
+	if obtenido["code"].(int) != 0 {
+		t.Errorf("Error obtenido modificando usuario, %q", obtenido["err"])
+	}
+
+	obtenido = bd.ObtenerUsuario(1, nuevaClave)
+	coincide, esperado := coincideUsuario(nuevoNombre, nuevoCorreo, nuevaClave, recibeCorreosTest, 1, obtenido)
+	if !coincide {
+		t.Errorf("CrearCuenta() = %q, se esperaba %q", obtenido, esperado)
+	}
+	bd.Cerrar()
 }
 
 func coincideUsuario(nombre, correo, clave string,
