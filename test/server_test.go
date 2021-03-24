6cbba312320ee4d5231aa5e7ada12ce778267d62
Samulix20 <56393807+Samulix20@users.noreply.github.com>
2021-03-24 22:28:19 +0100

    Update server_test.go

--- test/server_test.go
+++ test/server_test.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	// Pon aqui la url de la base de datos de Heroku
-	baseDeDatos       = "postgres://ehoyoqkpcgqyrc:c0f640db53a47820e660f19d987b013d7b01d9ac217b669f971070926969dfe0@ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d3cnglhgpo3rgk"
+	baseDeDatos       = "..."
 	nombreTest        = "usuario0"
 	correoTest        = "correo0@mail.com"
 	nombreAmigo1      = "nombre1"
